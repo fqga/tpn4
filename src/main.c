ef43a23c80479b08f4a1c06ce975147360a18341
Facundo Q <facundoqga@gmail.com>
2023-02-09 17:34:15 -0300

    fifth

--- src/main.c
+++ src/main.c
@@ -2,7 +2,7 @@
  * Facultad de Ciencias Exactas y Tecnología
  * Universidad Nacional de Tucuman
  * http://www.microprocesadores.unt.edu.ar/
- * Copyright 2022, Esteban Volentini <evolentini@herrera.unt.edu.ar>
+ * Copyright 2022, Facundo Quiroga <facundoqga@gmail.com>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
